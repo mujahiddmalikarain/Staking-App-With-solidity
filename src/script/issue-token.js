99bf78dd1c7168d99aeee5f142a0578d77800233
Mujahid Ramzan <mujahidmalikarain@gmail.com>
2023-03-12 05:22:06 -0700

    first commit

---
+++ src/script/issue-token.js
@@ -0,0 +1,13 @@
+const { artifacts } = require("truffle");
+const StakingDapp = artifacts.require('Staking_Dapp')
+
+module.exports = async function(callback){
+
+    let stakingdapp = await StakingDapp.deployed()
+    await stakingdapp.issuedummy()
+
+    console.log("dummy tokens issued!")
+    callback()
+
+
+}
